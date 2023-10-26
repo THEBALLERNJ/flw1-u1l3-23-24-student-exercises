0dff93471c93b9781f83dc826b33540fdc580c80
THEBALLERNJ <131172389+THEBALLERNJ@users.noreply.github.com>
2023-10-26 22:24:18 +0000

    completed

--- operators.js
+++ operators.js
@@ -5,19 +5,24 @@
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
+let num1 = 5
+let num2 = 6
+let add = num1 + num2
+console.log(add)
 
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
+let subtract = num2 - num1
+console.log(subtract)
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
-
+let multiply = num1 * num2
+console.log(multiply)
 
 // String Operations
 
@@ -25,4 +30,8 @@
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
+let word1 = "auto"
+let word2 = "motive"
 
+let combinedString = word1 + word2
+console.log(combinedString)
